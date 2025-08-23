eaa110e8d5fa6f0f0509643b8141e43efb74a828
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-08-24 04:18:12 +0900

    create the base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::io::Read;
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("Body:\n{}", body);
+
+    Ok(())
 }
