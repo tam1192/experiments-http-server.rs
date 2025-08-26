6baf6bf46ceadf26eb0e378b27d0090fce9936ec
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-08-27 04:13:56 +0900

    from_str対応

--- src/http_util/path/tests.rs
+++ src/http_util/path/tests.rs
@@ -3,22 +3,24 @@ use super::*;
 #[test]
 fn success_http_path_from_str() {
     assert_eq!(
-        HttpPath::from("/path/to/resource").to_string(),
+        HttpPath::from_str("/path/to/resource").unwrap().to_string(),
         String::from("/path/to/resource")
     );
     assert_eq!(
-        HttpPath::from("/path/to/resource?query=1").to_string(),
+        HttpPath::from_str("/path/to/resource?query=1")
+            .unwrap()
+            .to_string(),
         String::from("/path/to/resource?query=1")
     );
-    assert_eq!(HttpPath::from("/").to_string(), String::from("/"));
+    assert_eq!(
+        HttpPath::from_str("/").unwrap().to_string(),
+        String::from("/")
+    );
 }
 
 #[test]
 fn failure_http_path_from_str() {
-    assert_eq!(HttpPath::from_str(String::from("")), None);
-    assert_eq!(
-        HttpPath::from_str(String::from("/path/to/resource/*")),
-        None
-    );
-    assert_eq!(HttpPath::from_str(String::from("path/to/resource")), None);
+    assert!(HttpPath::from_str("").is_err());
+    assert!(HttpPath::from_str("/path/to/resource/*").is_err());
+    assert!(HttpPath::from_str("path/to/resource").is_err());
 }
