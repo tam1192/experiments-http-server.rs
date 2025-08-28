698585b4cd4e6f71444633c5a4bc2d2f2aa15a98
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-08-28 11:40:19 +0900

    mime実装途中

--- src/http_util/enums/mod.rs
+++ src/http_util/enums/mod.rs
@@ -1,6 +1,10 @@
 use core::fmt;
+use std::str::FromStr;
 
 use strum::EnumProperty;
+
+use crate::http_util::utils::line_parse_http_header;
+use anyhow::bail;
 
 #[cfg(test)]
 mod tests;
@@ -69,25 +73,39 @@ impl fmt::Display for Status {
     }
 }
 
-pub mod MIME {
-    pub enum Type {
-        Text(Text),
-        Application,
-    }
-    pub enum Text {
-        Plain,
-        Html,
-    }
-    pub enum Application {
-        Json,
-    }
+#[derive(Debug, Clone, PartialEq, Eq, strum_macros::Display, strum::EnumString)]
+pub enum MIME {
+    #[strum(serialize = "application/json")]
+    ApplicationJson,
+    #[strum(serialize = "text/plain")]
+    TextPlain,
+    #[strum(serialize = "text/html")]
+    TextHtml,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, strum_macros::Display, strum::EnumString)]
 pub enum Charset {
+    #[strum(serialize = "utf-8")]
     UTF8,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Header {
-    ContentType { mime: MIME::Type, charset: Charset },
+    ContentType { mime: MIME, charset: Charset },
     Other(String),
+}
+
+impl FromStr for Header {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let Some((k, v)) = line_parse_http_header(s) else {
+            bail!("error")
+        };
+
+        Ok(match k {
+            "Content-Type" | "content-type" => {}
+            _ => Self::Other(format!("{}:{}", k, v)),
+        })
+    }
 }
